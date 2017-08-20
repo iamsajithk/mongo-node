76f39f00953a62581aa540153b257681a1d43528
Sajith K <sajithmenon93@gmail.com>
2017-08-20 16:44:43 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 4000,
+mongoose = require('mongoose'),
+Task = require('./api/models/model'), //created model loading here
+bodyParser = require('body-parser');
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://root:Sajith031%40%23@test-shard-00-00-n8wq5.mongodb.net:27017,test-shard-00-01-n8wq5.mongodb.net:27017,test-shard-00-02-n8wq5.mongodb.net:27017/Test?ssl=true&replicaSet=Test-shard-0&authSource=admin'); 
+mongoose.connect('mongodb://localhost:27017/tasks'); 
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+var routes = require('./api/routes/routes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+console.log('todo list RESTful API server started on: ' + port);
